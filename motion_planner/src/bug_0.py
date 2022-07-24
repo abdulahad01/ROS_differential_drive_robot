1f6a2365ebe81b0f744aa642794a9cfcc21a35b7
Abdul Ahad <52212611+abdulahad01@users.noreply.github.com>
2022-07-24 13:47:16 +0530

    Update bug_0.py
    Added comments and improved readability

--- motion_planner/src/bug_0.py
+++ motion_planner/src/bug_0.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
+
+# import mesaage formats
+from geometry_msgs.msg import Point
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from tf import transformations
+
 import math
+
+# import service message of type setBool
 from std_srvs.srv import SetBool
 
+# Initialize necessary global variables
 ranges = {'right':0,
           'front':0,
           'left':0}
 _state = 0
-pub = None
-wall_follow_client_srv = None
-go_to_goal_client_srv = None
-
-vel_angular = 0.3
 x =0
 y =0
 yaw =0
 
+pub = None
+
+wall_follow_client_srv = None
+go_to_goal_client_srv = None
+
+# get goal position from goal param
+goal_position = Point()
+goal_position.x = rospy.get_param('goal_x')
+goal_position.y = rospy.get_param('goal_y')
+goal_position.z = 0
+
+# call back function for odom subscriber
 def odom_callback(msg):
     global yaw,x,y
     pos = msg.pose.pose.position
@@ -30,8 +43,7 @@ def odom_callback(msg):
     euler = transformations.euler_from_quaternion(qtrn)
     r,p,yaw=euler
 
-
-
+# call back function for laser scans
 def laser_callback(scans):
     global ranges
     # 720/3
@@ -39,11 +51,13 @@ def laser_callback(scans):
               'right' : min(min(scans.ranges[0:239]),10),
               'front' : min(min(scans.ranges[240:479]),10),
               'left' : min(min(scans.ranges[480:719]),10)}
-    # rospy.loginfo(ranges)
 
+#  state machine function
 def change_state(state):
     global _state
     _state = state
+    # 0 : go to goal
+    # 1 : wall follow
     if state ==  0:
         resp = go_to_goal_client_srv(True)
         resp = wall_follow_client_srv(False)
@@ -51,57 +65,67 @@ def change_state(state):
         resp = go_to_goal_client_srv(False)
         resp = wall_follow_client_srv(True)
 
-
-
+# main function all the behavioral logic goes here
 def main():
+    global y, x,yaw,ranges,goal_position, _state
+    global wall_follow_client_srv,go_to_goal_client_srv
 
     scan_topic = "/diff_robot/laser/scan"
-    vel_topic = "/cmd_vel"
     odom_topic = "/odom"
 
     rospy.init_node('bug_0',anonymous=True)
 
-    pub = rospy.Publisher(vel_topic,Twist,queue_size=10)
     sub_odoms = rospy.Subscriber(odom_topic,Odometry,odom_callback)
     sub_scans = rospy.Subscriber(scan_topic, LaserScan, laser_callback)
+
+    rospy.wait_for_service('/go_to_goal_switch')
+    rospy.wait_for_service('/wall_follower_switch')
+
+    wall_follow_client_srv = rospy.ServiceProxy('/wall_follower_switch',SetBool)
+    go_to_goal_client_srv = rospy.ServiceProxy('/go_to_goal_switch',SetBool)
+
     rate = rospy.Rate(10)
 
-    msg = Twist()
+    change_state(0)
 
     print("The program is running ....")
-    while not rospy.is_shutdown():
-        if  _state == None:
-            continue
-        
+    while not rospy.is_shutdown():        
         if  _state == 0:
             if ranges['front'] > 0.15 and ranges['front'] < 1:
                 change_state(1)
+                rospy.loginfo("FOllowing wall")
         
         elif _state == 1:
-            desired_yaw = math.atan2(desired_position_.y - position_.y, desired_position_.x - position_.x)
-            err_yaw = normalize_angle(desired_yaw - yaw_)
+            heading = math.atan2(goal_position.y-y,goal_position.x-x)
+            err_yaw = heading- yaw
+            print
             
             # less than 30 degrees
             if math.fabs(err_yaw) < (math.pi / 6) and \
-             ranges['front'] > 1.5 and ranges['fright'] > 1 and ranges['fleft'] > 1:
+             ranges['front'] > 1.5 and ranges['right'] > 1 and ranges['left'] > 1:
                 change_state(0)
+                rospy.loginfo("going to goal")
             
             # between 30 and 90
             if err_yaw > 0 and \
                math.fabs(err_yaw) > (math.pi / 6) and \
                math.fabs(err_yaw) < (math.pi / 2) and \
-             ranges['left'] > 1.5 and ranges['fleft'] > 1:
+             ranges['left'] > 1.5 and ranges['left'] > 1:
                 change_state(0)
+                rospy.loginfo("going to goal")
+
                 
             if err_yaw < 0 and \
                math.fabs(err_yaw) > (math.pi / 6) and \
                math.fabs(err_yaw) < (math.pi / 2) and \
-             ranges['right'] > 1.5 and ranges['fright'] > 1:
+             ranges['right'] > 1.5 and ranges['right'] > 1:
                 change_state(0)            
+                rospy.loginfo("going to goal")
+
         rate.sleep()
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     try:
         main()
     except:
