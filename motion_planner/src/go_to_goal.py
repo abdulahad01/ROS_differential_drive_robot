ffffa944170dc8db2868a96abbe546e4e7c456e4
Abdul Ahad <52212611+abdulahad01@users.noreply.github.com>
2022-07-24 13:48:42 +0530

    Update go_to_goal.py
    added service callbacks and formatted code

--- motion_planner/src/go_to_goal.py
+++ motion_planner/src/go_to_goal.py
@@ -1,57 +1,85 @@
 #!/usr/bin/env python
-from numpy import arctan2
+
 import rospy 
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point
 from tf import transformations
+from std_srvs.srv import *
 import math
 
 vel_angular = 0.3
 x =0
 y =0
 yaw =0
+active = False
+pub = None
+
+position = Point()
+
+desired_position = Point()
+desired_position.x = rospy.get_param('goal_x')
+desired_position.y = rospy.get_param('goal_y')
+desired_position.z = 0
+
+def gtg_switch(req):
+    global active
+    active = req.data
+    res = SetBoolResponse()
+    res.success = True
+    res.message = 'Done'
+    return res
 
 def odom_callback(msg):
-    global yaw,x,y
-    pos = msg.pose.pose.position
-    x = pos.x
-    y = pos.y
+    global yaw,position
+    position = msg.pose.pose.position
+
     qtrn = [msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z,msg.pose.pose.orientation.w]
     euler = transformations.euler_from_quaternion(qtrn)
     r,p,yaw=euler
 
-
-
-def go_to_goal(x_goal,y_goal):
-    global y, x,yaw
+def go_to_goal(point):
+    global position,yaw
+    x = position.x
+    y = position.y
+    x_goal = point.x
+    y_goal = point.y
     K_linear  = 0.1
     K_angular = 0.25
     vel = Twist()
-    pub = rospy.Publisher("cmd_vel",Twist,queue_size=20)
     while 1:
         dist = math.sqrt(math.pow(y_goal-y,2)+math.pow(x_goal-x,2))
         heading = math.atan2(y_goal-y,x_goal-x)
         error = heading- yaw
         vel.angular.z= K_angular* error
         vel.linear.x = K_linear*dist
-        pub.publish(vel)
         if dist<0.1:
             break
+        return vel
     vel.angular.z= 0
     vel.linear.x = 0
+    return vel
         
-    
-
 
 def main():
-    # global pub
+    global pub,desired_position,active
+
     rospy.init_node("go_to_goal",anonymous=True)
+
     sub = rospy.Subscriber("/odom",Odometry,odom_callback)
-    # pub = rospy.Publisher("cmd_vel",Twist,queue_size=20)
-    print("Initialized")
-    go_to_goal(1,1)
-    rospy.spin()
+    pub = rospy.Publisher('/cmd_vel',Twist,queue_size =10)
+    srv = rospy.Service("/go_to_goal_switch",SetBool,gtg_switch)
+
+    rate = rospy.Rate(10)
+
+    print("Running go to goal")
+    while not rospy.is_shutdown():
+        if not active:
+            continue
+        else:
+            msg = go_to_goal(desired_position)
+            pub.publish(msg)
+        rate.sleep()
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
